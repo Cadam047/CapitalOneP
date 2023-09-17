97430b221a3050432a712160208428eb3cac404d
Cadam047 <cadam047@fiu.edu>
2023-09-17 10:27:38 -0400

    new2

---
+++ AIscript.js
@@ -0,0 +1,63 @@
+const API_KEY = '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'
+const submitButton = document.querySelector('#submit')
+const outPutElement = document.querySelector('#output')
+const inputElement = document.querySelector('input')
+const historyElement = document.querySelector('.history')
+const buttonElement = document.querySelector('button')
+
+function changeInput(value) {
+
+  const inputElement = document.querySelector('input')
+  inputElement.value = value
+}
+
+async function getMessage() {
+  console.log('clicked')
+  const options = {
+
+    method: 'POST',
+    headers: {
+
+      'Authorization': `Bearer ${API_KEY}`,
+      'Content-Type': 'application/json'
+       
+    },
+    body: JSON.stringify({
+
+      model: "gpt-3.5-turbo",
+      messages: [{role: "user", content: inputElement.value}],
+      max_tokens: 100
+    })
+  }
+  
+
+  try {
+
+    const response = await fetch('https://api.openai.com/v1/chat/completions', options)
+    const data = await response.json()
+    console.log(data)
+    outPutElement.textContent = data.choices[0].message.content
+      if (data.choices[0].message.content && inputElement.value) {
+      const pElement = document.createElement('p')
+      pElement.textContent = inputElement.value
+      pElement.addEventListener('click', () => changeInput(pElement.textContent))
+      historyElement.append(pElement) 
+    }
+  } catch (error){
+
+    console.error(error)
+    
+  }
+
+  
+}
+
+submitButton.addEventListener('click', getMessage)
+
+function clearInput() {
+
+  inputElement.value = ''
+  
+}
+
+buttonElement.addEventListener('click', clearInput)
